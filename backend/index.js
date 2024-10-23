003da87415f534585135b36d77b377184eeec43a
Monique Matamoros <tiffanymatamoros72@gmail.com>
2024-10-23 17:23:31 -0600

    feat: :construction_worker: add tsconfig

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
